9e9260ddd52c107f6dafd767ad57c62a9e270b96
mind_blade <kanmear+github@gmail.com>
2023-05-04 21:31:49 +0300

    feat: top nav bar => bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,66 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
         create: (context) => AppState(),
-        child: MaterialApp(
-          home: DefaultTabController(
-            length: 2,
-            child: Scaffold(
-              appBar: AppBar(
-                bottom: const TabBar(tabs: [
-                  Tab(icon: Icon(Icons.add)),
-                  Tab(icon: Icon(Icons.bar_chart))
-                ]),
-                title: const Text('Tabs'),
-              ),
-              body: const TabBarView(children: [EntriesPage(), StatsPage()]),
-            ),
-          ),
+        child: const MaterialApp(
+          home: Budgetron(),
         ));
+  }
+}
+
+class Budgetron extends StatefulWidget {
+  const Budgetron({
+    super.key,
+  });
+
+  @override
+  State<Budgetron> createState() => _BudgetronState();
+}
+
+class _BudgetronState extends State<Budgetron> {
+  final pageViewController = PageController(initialPage: 1);
+  int selectedIndex = 1;
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
+
+  void selectPage(index) {
+    pageViewController.animateToPage(index,
+        duration: const Duration(milliseconds: 200), curve: Curves.ease);
+  }
+
+  void updateIndex(index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pageViewController,
+        children: const [
+          EntriesPage(),
+          Text("Home page placeholder"),
+          StatsPage()
+        ],
+        onPageChanged: (index) => updateIndex(index),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selectedIndex,
+          onTap: selectPage,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Entries'),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Stats')
+          ]),
+      appBar: AppBar(
+        title: const Text('Budgetron a0.1'),
+      ),
+    );
   }
 }
 
