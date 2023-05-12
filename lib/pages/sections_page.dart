ad26ede95ba8fbaf91b87c989eb894a22a53d34e
mind_blade <kanmear+github@gmail.com>
2023-05-12 13:52:16 +0300

    add appbar for categories page

--- lib/pages/sections_page.dart
+++ lib/pages/sections_page.dart
@@ -15,36 +15,38 @@ class SectionPage extends StatelessWidget {
     var appState = context.watch<AppState>();
 
     return Scaffold(
-        body: StreamBuilder<List<Section>>(
-            stream: objectBox.getSections(),
-            builder: (context, snapshot) {
-              if (snapshot.data?.isNotEmpty ?? false) {
-                return ListView(
-                  children: [
-                    // rename entry to section
-                    for (var section in snapshot.data!)
-                      Card(
-                          child: ListTile(
-                              leading: section.isExpense
-                                  ? const Icon(Icons.money_off)
-                                  : const Icon(Icons.money),
-                              title: Text(section.name),
-                              onTap: () => selectSectionAndReturn(
-                                  appState, section, context)))
-                  ],
-                );
-              } else {
-                return const Center(
-                  child: Text("No sections in database"),
-                );
-              }
-            }),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => showDialog(
-              context: context,
-              builder: (BuildContext context) => const NewSectionDialog()),
-          child: const Icon(Icons.add),
-        ));
+      body: StreamBuilder<List<Section>>(
+          stream: objectBox.getSections(),
+          builder: (context, snapshot) {
+            if (snapshot.data?.isNotEmpty ?? false) {
+              return ListView(
+                children: [
+                  // rename entry to section
+                  for (var section in snapshot.data!)
+                    Card(
+                        child: ListTile(
+                            leading: section.isExpense
+                                ? const Icon(Icons.money_off)
+                                : const Icon(Icons.money),
+                            title: Text(section.name),
+                            onTap: () => selectSectionAndReturn(
+                                appState, section, context)))
+                ],
+              );
+            } else {
+              return const Center(
+                child: Text("No sections in database"),
+              );
+            }
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showDialog(
+            context: context,
+            builder: (BuildContext context) => const NewSectionDialog()),
+        child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(title: const Text('Categories')),
+    );
   }
 
   selectSectionAndReturn(
