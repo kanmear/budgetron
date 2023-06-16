061879b1633331d3c5d918463824c22cecbeb62e
mind_blade <kanmear+github@gmail.com>
2023-06-16 23:43:00 +0300

    refactor: extract checkbox ui

---
+++ lib/ui/classes/checkbox.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ExpenseCheckbox extends StatefulWidget {
+  final ValueNotifier<bool> valueNotifier;
+
+  const ExpenseCheckbox({super.key, required this.valueNotifier});
+  @override
+  State<ExpenseCheckbox> createState() => _ExpenseCheckboxState();
+}
+
+class _ExpenseCheckboxState extends State<ExpenseCheckbox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.red;
+      } else {
+        return Colors.blue;
+      }
+    }
+
+    return Checkbox(
+      value: isChecked,
+      checkColor: Colors.white,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+          widget.valueNotifier.value = isChecked;
+        });
+      },
+    );
+  }
+}
