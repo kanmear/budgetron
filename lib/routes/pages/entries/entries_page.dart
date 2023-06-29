d4bc74a938616c1845c2d4ce3ddf30cfe1d9b640
mind_blade <kanmear+github@gmail.com>
2023-06-29 08:56:12 +0300

    refactor: ui entity logical separation

--- lib/routes/pages/entries/entries_page.dart
+++ lib/routes/pages/entries/entries_page.dart
@@ -69,91 +69,117 @@ class EntriesListView extends StatelessWidget {
       itemBuilder: (context, index) {
         var day = entryDates[index];
 
-        return Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-              child: Card(
-                elevation: 3,
-                shadowColor: entriesMap.keys.first == day
-                    ? const Color.fromARGB(25, 0, 0, 0)
-                    : Colors.transparent,
-                color: Theme.of(context).colorScheme.surface,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 10.0, right: 10.0, top: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            (entriesMap.keys.first == day &&
-                                    day ==
-                                        DateTime(
-                                            DateTime.now().year,
-                                            DateTime.now().month,
-                                            DateTime.now().day))
-                                ? "Today"
-                                : DateFormat.yMMMd().format(day),
-                            style: BudgetronFonts.nunitoSize16Weight600,
-                          ),
-                          Text(
-                            entriesMap[day]!
-                                .map((e) => e.value)
-                                .reduce((value, element) => value + element)
-                                .toString(),
-                            style: BudgetronFonts.nunitoSize16Weight600,
-                          )
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Column(children: [
-                      for (var entry in entriesMap[day]!)
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(left: 8.0, right: 10.0),
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Icon(
-                                        Icons.square_rounded,
-                                        size: 18,
-                                        color: Color(int.parse(
-                                            radix: 16,
-                                            entry.category.target!.color)),
-                                      ),
-                                      const SizedBox(width: 8),
-                                      Text(entry.category.target!.name,
-                                          style: BudgetronFonts
-                                              .nunitoSize16Weight400)
-                                    ],
-                                  ),
-                                  Text(
-                                    entry.value.toString(),
-                                    style: BudgetronFonts.nunitoSize16Weight400,
-                                  )
-                                ],
-                              ),
-                              const SizedBox(height: 8.0)
-                            ],
-                          ),
-                        ),
-                    ]),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 24)
-          ],
-        );
+        return EntryListTileContainer(entriesMap: entriesMap, day: day);
       },
+    );
+  }
+}
+
+class EntryListTileContainer extends StatelessWidget {
+  const EntryListTileContainer({
+    super.key,
+    required this.entriesMap,
+    required this.day,
+  });
+
+  final Map<DateTime, List<Entry>> entriesMap;
+  final DateTime day;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+          child: Card(
+            elevation: 3,
+            shadowColor: entriesMap.keys.first == day
+                ? const Color.fromARGB(25, 0, 0, 0)
+                : Colors.transparent,
+            color: Theme.of(context).colorScheme.surface,
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        (entriesMap.keys.first == day &&
+                                day ==
+                                    DateTime(
+                                        DateTime.now().year,
+                                        DateTime.now().month,
+                                        DateTime.now().day))
+                            ? "Today"
+                            : DateFormat.yMMMd().format(day),
+                        style: BudgetronFonts.nunitoSize16Weight600,
+                      ),
+                      Text(
+                        entriesMap[day]!
+                            .map((e) => e.value)
+                            .reduce((value, element) => value + element)
+                            .toString(),
+                        style: BudgetronFonts.nunitoSize16Weight600,
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Column(children: [
+                  for (var entry in entriesMap[day]!)
+                    EntryListTile(entry: entry),
+                ]),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: 24)
+      ],
+    );
+  }
+}
+
+class EntryListTile extends StatelessWidget {
+  const EntryListTile({
+    super.key,
+    required this.entry,
+  });
+
+  final Entry entry;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 10.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.square_rounded,
+                    size: 18,
+                    color: Color(
+                        int.parse(radix: 16, entry.category.target!.color)),
+                  ),
+                  const SizedBox(width: 8),
+                  Text(entry.category.target!.name,
+                      style: BudgetronFonts.nunitoSize16Weight400)
+                ],
+              ),
+              Text(
+                entry.value.toString(),
+                style: BudgetronFonts.nunitoSize16Weight400,
+              )
+            ],
+          ),
+          const SizedBox(height: 8.0)
+        ],
+      ),
     );
   }
 }
