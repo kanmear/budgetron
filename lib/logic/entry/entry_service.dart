8ec3707198354e1ab4486dd5bdcd79ddd8b76c67
mind_blade <kanmear+github@gmail.com>
2023-07-05 22:46:25 +0300

    refactor: collapse same-date entries with identical categories

--- lib/logic/entry/entry_service.dart
+++ lib/logic/entry/entry_service.dart
@@ -15,8 +15,24 @@ class EntryService {
       Map<DateTime, List<Entry>> entriesMap, Entry entry) {
     DateTime dateTime =
         DateTime(entry.dateTime.year, entry.dateTime.month, entry.dateTime.day);
+
     if (entriesMap.containsKey(dateTime)) {
-      entriesMap.update(dateTime, (value) => List.from(value)..addAll({entry}));
+      List<Entry> currentEntries = entriesMap[dateTime]!;
+
+      // if an entry with the same category exists - update that entry
+      int sameCategoryEntry = currentEntries.indexWhere((element) =>
+          element.category.target!.name == entry.category.target!.name);
+      if (sameCategoryEntry != -1) {
+        Entry updatedEntry = currentEntries[sameCategoryEntry];
+        updatedEntry.value += entry.value;
+        currentEntries[sameCategoryEntry] = updatedEntry;
+      }
+
+      // if category is unique for that date - add as a separate row
+      else {
+        entriesMap.update(
+            dateTime, (value) => List.from(value)..addAll({entry}));
+      }
     } else {
       entriesMap[dateTime] = List.from({entry});
     }
