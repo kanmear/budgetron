37197b947d63f1f3ad8f9f69a7d3d16042b243e2
mind_blade <kanmear+github@gmail.com>
2023-07-08 14:22:18 +0300

    refactor: extract docked dialog as a generic widget

---
+++ lib/ui/classes/docked_popup.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:budgetron/ui/fonts.dart';
+
+class DockedDialog extends StatelessWidget {
+  final String title;
+  final Widget body;
+
+  const DockedDialog({super.key, required this.title, required this.body});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      alignment: Alignment.bottomCenter,
+      insetPadding: EdgeInsets.zero,
+      contentPadding:
+          const EdgeInsets.only(right: 16, left: 16, top: 16, bottom: 14),
+      content: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title, style: BudgetronFonts.nunitoSize18Weight600),
+              IconButton(
+                constraints: const BoxConstraints(),
+                padding: EdgeInsets.zero,
+                icon: const Icon(Icons.close),
+                //TODO dismiss keyboard with FocusManager.instance.primaryFocus?.unfocus()
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+          ),
+          const SizedBox(height: 24),
+          body,
+        ]),
+      ),
+    );
+  }
+}
