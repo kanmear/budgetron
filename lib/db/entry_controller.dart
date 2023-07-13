e93c00630290f61d5e47a6de1e5cff752d1315d5
mind_blade <kanmear+github@gmail.com>
2023-07-13 19:07:58 +0300

    feat: rewrite entry querying to allow filtering by categories

--- lib/db/entry_controller.dart
+++ lib/db/entry_controller.dart
@@ -4,17 +4,25 @@ import 'package:budgetron/models/entry.dart';
 import 'package:budgetron/models/category.dart';
 
 class EntryController {
-  static Stream<List<Entry>> getEntries(List<DateTime>? period) {
+  static Stream<List<Entry>> getEntries(
+      {List<DateTime>? period, List<EntryCategory>? categoryFilter}) {
     QueryBuilder<Entry> queryBuilder;
+    Condition<Entry>? condition;
+
     if (period != null) {
-      queryBuilder = objectBox.entryBox.query(
+      condition =
           Entry_.dateTime.greaterOrEqual(period[0].millisecondsSinceEpoch) &
-              Entry_.dateTime.lessOrEqual(period[1].millisecondsSinceEpoch))
-        ..order(Entry_.id, flags: Order.descending);
-    } else {
-      queryBuilder = objectBox.entryBox.query()
-        ..order(Entry_.id, flags: Order.descending);
+              Entry_.dateTime.lessOrEqual(period[1].millisecondsSinceEpoch);
     }
+
+    queryBuilder = objectBox.entryBox.query(condition);
+
+    if (categoryFilter != null) {
+      queryBuilder.link(Entry_.category,
+          EntryCategory_.id.oneOf(categoryFilter.map((e) => e.id).toList()));
+    }
+
+    queryBuilder.order(Entry_.id, flags: Order.descending);
 
     return queryBuilder
         .watch(triggerImmediately: true)
