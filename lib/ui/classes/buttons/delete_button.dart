72ea89c203ae8b7123ed8765aeb26e079349c4f0
mind_blade <kanmear+github@gmail.com>
2023-10-18 22:26:36 +0300

    feat: ui -> delete icon button

---
+++ lib/ui/classes/buttons/delete_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DeleteButton extends StatelessWidget {
+  final Function onTap;
+
+  const DeleteButton({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.error,
+            borderRadius: const BorderRadius.all(Radius.circular(3))),
+        height: 48,
+        width: 48,
+        child: IconButton(
+          icon: const Icon(Icons.delete, color: Colors.white),
+          onPressed: () => onTap(),
+        ));
+  }
+}
