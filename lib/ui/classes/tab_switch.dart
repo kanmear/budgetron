f441fb91252a56a8a1cc8b461a3e69d62a7abfcd
mind_blade <kanmear+github@gmail.com>
2024-09-19 23:45:13 +0300

    feat: tab selector -> custom animation

--- lib/ui/classes/tab_switch.dart
+++ lib/ui/classes/tab_switch.dart
@@ -1,5 +1,8 @@
-import 'package:budgetron/ui/data/colors.dart';
 import 'package:flutter/material.dart';
+
+import 'package:budgetron/ui/data/colors.dart';
+
+import 'package:budgetron/utils/string_utils.dart';
 
 class BudgetronTabSwitch extends StatelessWidget {
   final ValueNotifier<Enum> valueNotifier;
@@ -17,14 +20,93 @@ class BudgetronTabSwitch extends StatelessWidget {
 
     return Padding(
         padding: const EdgeInsets.only(left: 16, right: 16),
-        child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: theme.colorScheme.surfaceContainerLowest),
-            child: Row(children: [
+        child: Stack(
+          children: [
+            const BackgroundContainer(),
+            AnimatedTabSlide(
+              valueNotifier: valueNotifier,
+              firstTab: tabs.first,
+            ),
+            Row(children: [
               for (var tab in tabs)
                 SwitchTab(valueNotifier: valueNotifier, tab: tab, theme: theme)
-            ])));
+            ])
+          ],
+        ));
+  }
+}
+
+//REFACTOR a better way to handle this?
+class BackgroundContainer extends StatelessWidget {
+  const BackgroundContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: theme.colorScheme.surfaceContainerLowest),
+      child: Row(
+        children: [
+          for (var _ in [0, 0])
+            //NOTE this mimics how actual tabs are made to match size
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(4),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 6, bottom: 6),
+                    child: Text(
+                      StringUtils.emptyString,
+                      style: theme.textTheme.headlineMedium!,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class AnimatedTabSlide extends StatelessWidget {
+  final ValueNotifier<Enum> valueNotifier;
+  final Enum firstTab;
+
+  const AnimatedTabSlide({
+    super.key,
+    required this.valueNotifier,
+    required this.firstTab,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: valueNotifier,
+      builder: (context, value, _) {
+        return AnimatedPositioned(
+          duration: const Duration(milliseconds: 150),
+          curve: Curves.easeInOut,
+          left: valueNotifier.value == firstTab
+              ? 0
+              : MediaQuery.of(context).size.width / 2 - 16,
+          top: 0,
+          bottom: 0,
+          child: Padding(
+            padding: const EdgeInsets.all(4),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: BudgetronColors.darkSurface0),
+              width: MediaQuery.of(context).size.width / 2 - 24,
+            ),
+          ),
+        );
+      },
+    );
   }
 }
 
@@ -49,34 +131,29 @@ class SwitchTab extends StatelessWidget {
             child: ValueListenableBuilder(
                 valueListenable: valueNotifier,
                 builder: (context, value, Widget? child) {
-                  bool isSelected = tab == valueNotifier.value;
                   return Padding(
                       padding: const EdgeInsets.all(4),
                       child: Container(
                           decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(4),
-                              color: _resolveTabColor(isSelected)),
+                              borderRadius: BorderRadius.circular(4)),
                           child: Center(
                             child: Padding(
                                 padding:
                                     const EdgeInsets.only(top: 6, bottom: 6),
-                                child: Text(
-                                  tab.toString(),
+                                child: AnimatedDefaultTextStyle(
+                                  duration: const Duration(milliseconds: 150),
+                                  curve: Curves.easeInOut,
                                   style: theme.textTheme.headlineMedium!.apply(
-                                      color: _resolveTextColor(isSelected)),
+                                      color: value == tab
+                                          ? BudgetronColors.darkPrimary
+                                          : theme.colorScheme.primary),
+                                  child: Text(tab.toString()),
                                 )),
                           )));
                 })));
   }
 
-  void _selectTab(Enum value) => valueNotifier.value = value;
-
-  Color _resolveTabColor(bool isSelected) => isSelected
-      ? BudgetronColors.darkSurface0
-      : theme.colorScheme.surfaceContainerLowest;
-
-  Color _resolveTextColor(bool isSelected) =>
-      isSelected ? BudgetronColors.darkPrimary : theme.colorScheme.primary;
+  _selectTab(Enum value) => valueNotifier.value = value;
 }
 
 class BudgetronDisabledTabSwitch extends StatelessWidget {
